42ad8e6842d1544b25ca728537cd9a9843f87f35
dazzammmkay1 <dar.ren.keane@gmail.com>
2023-06-14 05:15:40 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -201,6 +201,12 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+
+module "azure-backup" {
+  source  = "ravensorb/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-public"
+}
 module "backupstorage" {
   source = "app.terraform.io/dazzammmkaytrial/azure-backup/azurerm"
   version = "1.0.2"
